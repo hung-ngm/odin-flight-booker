4f1a2dcb1bf77d9938e86d3c164aabeb683d60fd
Minh Hung Nguyen <prohungnguyenminh@gmail.com>
2021-03-15 20:19:42 +0700

    Create action to send thank you email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: "confirmation@gmail.com"
+  layout 'mailer'
+
+  def welcome_email(passenger)
+    @passenger = passenger
+    @url = "http://example.com/login"
+    mail(to: @passenger.email, subject: 'Welcome to Flight Booking')
+  end
 end
