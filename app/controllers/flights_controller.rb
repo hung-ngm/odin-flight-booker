87e4ce2193477cfb287dcea0942b7a7d9f5797ed
Minh Hung Nguyen <prohungnguyenminh@gmail.com>
2021-03-15 19:49:49 +0700

    Edit routes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,19 +1,20 @@
 class FlightsController < ApplicationController
   include FlightsHelper
   def index
-    @flights = Flight.all
-    @selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
+    if params.has_key?(:day) && params.has_key?(:month) && params.has_key?(:year)
+      selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
+    end
   end
 
-  def mew
-    @flight = Flight.new
-  end
+  # def new
+  #   @flight = Flight.new
+  # end
 
-  def create
-    @flight = Flight.new(flight_params)
-    @flight.save
-    flash[:notide] = "Your flight has been created"
-    redirect_to root_path
-  end
+  # def create
+  #   @flight = Flight.new(flight_params)
+  #   @flight.save
+  #   flash[:notide] = "Your flight has been created"
+  #   redirect_to root_path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html\
   root 'flights#index'
   resources :airports
-  resources :flights
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
 
 end
