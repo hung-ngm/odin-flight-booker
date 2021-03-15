089e6e861f00ef27a9c06442b9d747db6f11e75d
Minh Hung Nguyen <prohungnguyenminh@gmail.com>
2021-03-15 19:37:15 +0700

    Booking action with form model and nested attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,21 @@
 class BookingsController < ApplicationController
-  
+  def new
+    @booking = Booking.new
+    @selected_flight = Flight.find(params[:flight])
+    @passengers_count = params[:pax].to_i
+    @passengers_count.times{ @booking.passengers.build }
+
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:notice] = "Booking created successfully"
+      redirect_to booking_path(@booking)
+    else
+      flash[:alert] = "An error occurred"
+      render 'new'
+    end
+
+  end
 end
